b5b55b2af259df16399bd560d440f2efe08bc48e
Yosuke Adachi <doruji1982@gmail.com>
2022-10-10 23:11:14 +0900

    Decorator pattern

---
+++ Ch3Decorator/Ch3Decorator/IComponent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch3Decorator
+{
+    public interface IComponent
+    {
+        void Something();
+    }
+
+    internal class ConcreteComponent : IComponent
+    {
+        public void Something()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class DecoratorComponent : IComponent
+    {
+        private readonly IComponent _component;
+
+        public DecoratorComponent(IComponent component)
+        {
+            // パラメータとして受け取る
+            _component = component;
+        }
+        public void Something()
+        {
+            // 既存機能に追加する（decorate）機能
+            SomethingElse();
+            // 型の契約を満たす
+            _component.Something();
+        }
+
+        private void SomethingElse()
+        {
+        }
+    }
+}
---
+++ Ch3Decorator/Ch3Decorator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch3Decorator
+{
+    internal class Program
+    {
+        static IComponent component;
+        static void Main(string[] args)
+        {
+            component = new DecoratorComponent(new ConcreteComponent());
+            component.Something();
+        }
+    }
+}
