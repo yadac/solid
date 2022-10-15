5b55d2d4d9c3280a1208db54cec2df35676b51b3
Yosuke Adachi <doruji1982@gmail.com>
2022-10-15 23:47:08 +0900

    タスクリストDataGridを配置

---
+++ Ch9/Ch9.WPF/Domain/MyTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch9.WPF.Domain
+{
+    public class MyTask
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public PriorityLevel Priority { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool Completed { get; set; }
+
+        public MyTask(int id, string description
+            , PriorityLevel priority, DateTime dueDate, bool completed)
+        {
+            Id = id;
+            Description = description;
+            Priority = priority;
+            DueDate = dueDate;
+            Completed = completed;
+        }
+
+        public enum PriorityLevel
+        {
+            LOW,
+            MEDIUM,
+            HIGH
+        }
+    }
+}
---
+++ Ch9/Ch9.WPF/ViewModels/TaskListWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Ch9.WPF.Domain;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch9.WPF.ViewModels
+{
+    public class TaskListWindowViewModel
+    {
+        // notify機能付きcollectionなのでバインドしておくだけ
+        private ObservableCollection<MyTask> _myTasks;
+        public TaskListWindowViewModel()
+        {
+            _myTasks = new ObservableCollection<MyTask>
+            {
+                new MyTask(0, "Clean the house", MyTask.PriorityLevel.LOW, DateTime.Now, false),
+                new MyTask(1, "Pay the bills", MyTask.PriorityLevel.HIGH, DateTime.Now.AddDays(1), false),
+                new MyTask(2, "Wash the dog", MyTask.PriorityLevel.MEDIUM, DateTime.Now.AddDays(2), false),
+            };
+        }
+
+        public ObservableCollection<MyTask> MyTasks
+        {
+            get { return _myTasks; }
+        }
+    }
+}
